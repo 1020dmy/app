707f2dacab8bebfbc4998a930741369469f323b8
1020dmy <1747053052@qq.com>
2020-07-10 14:25:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initData();
+    }
+
+    private void initData() {
+        Toast.makeText(this, "上传gitbut", Toast.LENGTH_SHORT).show();
+    }
+
+}
